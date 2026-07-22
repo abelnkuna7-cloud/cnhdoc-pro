2b88b35e6b4644e96b0bc3425f91d9270d4872f2
abelnkuna7-cloud <abelnkuna7@gmail.com>
2026-07-22 22:40:13 +0200

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,26 @@
 import { createStart, createMiddleware } from "@tanstack/react-start";
-
 import { renderErrorPage } from "./lib/error-page";
+
+// Google Analytics
+const initGA = () => {
+  const script1 = document.createElement('script');
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=G-32E8QXJ40F`;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement('script');
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-32E8QXJ40F');
+  `;
+  document.head.appendChild(script2);
+};
+
+if (typeof document !== 'undefined') {
+  initGA();
+}
 
 const errorMiddleware = createMiddleware().server(async ({ next }) => {
   try {
