b6dacd2a2174e8063241db6b6a914c86f952958e
abelnkuna7-cloud <abelnkuna7@gmail.com>
2026-07-22 22:35:32 +0200

    Create analytics.ts

---
+++ src/lib/analytics.ts
@@ -0,0 +1,17 @@
+export const GA_MEASUREMENT_ID = 'G-32E8QXJ40F';
+
+export const initGA = () => {
+  const script1 = document.createElement('script');
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement('script');
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_MEASUREMENT_ID}');
+  `;
+  document.head.appendChild(script2);
+};
